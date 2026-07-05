10756df6d79e35eae4a6c4f39d8673c77f413f9c
Abhishek279-dot <smartabhi1213@gmail.com>
2026-07-05 13:22:08 +0530

    dark mode feature added

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
-const themeToggle = document.querySelector(".theme-toggle");
+// dark mode and dark mode btn switching 
 
-themeToggle.addEventListener("click", () => {
+
+const toggleBtn = document.querySelector(".theme-toggle");
+
+toggleBtn.addEventListener("click", () => {
+
     document.body.classList.toggle("dark-mode");
+
+    const themeIcon = document.getElementById("themeIcon");
+
+    if (document.body.classList.contains("dark-mode")) {
+        themeIcon.setAttribute("data-lucide", "sun");
+    } else {
+        themeIcon.setAttribute("data-lucide", "moon");
+    }
+
+    lucide.createIcons();
 });
