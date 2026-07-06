a2ddd75db64315df67dc73c9dc0f28ee9a014408
Abhishek279-dot <smartabhi1213@gmail.com>
2026-07-06 21:04:48 +0530

    dark mode local storage added

--- script.js
+++ script.js
@@ -3,16 +3,31 @@
 
 const toggleBtn = document.querySelector(".theme-toggle");
 
+const savedMode = localStorage.getItem("mode");
+
+if(savedMode === "dark"){
+    document.body.classList.add("dark-mode");
+}
+
+
+
 toggleBtn.addEventListener("click", () => {
 
     document.body.classList.toggle("dark-mode");
 
+
+
     const themeIcon = document.getElementById("themeIcon");
 
     if (document.body.classList.contains("dark-mode")) {
+        
         themeIcon.setAttribute("data-lucide", "sun");
+        localStorage.setItem("mode","dark");
+
     } else {
+
         themeIcon.setAttribute("data-lucide", "moon");
+        localStorage.setItem("mode","light");
     }
 
     lucide.createIcons();
